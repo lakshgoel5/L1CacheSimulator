d422e8c2662221279178c8d20fcbed86d082183c
Laksh Goel <lakshgoel541@gmail.com>
2025-04-05 11:56:52 +0000

    Adding functionality to Cache

--- src/headers/Cache.hpp
+++ src/headers/Cache.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <stdexcept>
+#include <cstdint>
 
 using namespace std;
 
@@ -13,26 +14,52 @@ enum State{
     M, E, S, I
 };
 
-class CacheLines{
+class CacheLines{ //CacheBlock
     private:
+    std::vector<int32_t> data; //Each CacheLine contains data
     State state;
-    unsigned int tag;
-    // bool valid;
+    uint32_t tag; //Every cache line is associated with a tag
+    bool valid; //For deciding if data is present in cache or not (initially false)
     // bool dirty;
-    // int lru;
+    uint32_t lru;
 
-    // public:
-    // CacheLines() : tag(0), valid(false), dirty(false), lru(0) {}
+    public:
+    CacheLines(size_t blockSize);
+    void setState(State state);
+    State getSTate();
+
+    void setTag(uint32_t tag);
+    uint32_t getTag();
+
+    bool isValid();
+    void setValid(bool valid);
+
+    void setLru(uint32_t lru);
+    uint32_t getLru();
+
+    void writeData(size_t offset, int32_t data);
+    int32_t readData(size_t offset);
+
+    void printCacheLine(); //debug
 };
 
 class CacheSet{
     private:
-    std::vector<CacheLines> cacheSet; //Each CacheSet contains lines
+    std::vector<CacheLines> cachelines_data; //Each CacheSet contains lines
+
+    public:
+    CacheSet(size_t numLines, size_t blockSize);
 };
 
 class Cache{
     private:
-    std::vector<CacheSet> cache; //Each Cache contains cache sets
+    std::vector<CacheSet> cacheset_data; //Each Cache contains cache sets
+    uint32_t numSets;
+    uint32_t numLinesPerSet;
+    uint32_t blockSize;
+
+    public:
+    Cache(size_t numSets, size_t numLines, size_t blockSize);
 };
 
 
